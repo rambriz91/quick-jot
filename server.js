3109e508d7eb0334b5be425c9edc7a85e4696d65
rambriz91 <rambriz91@gmail.com>
2023-09-15 19:49:59 -0700

    set up rootpath to index.html.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 
+//sets the rootpath to index.html
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () =>
 console.log(`Quick Jot listening at http://localhost:${PORT}`)
 );
